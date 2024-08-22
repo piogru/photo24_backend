42c8483adef40f8db66381403fafecfbbf3d020a
Piotr G <81086063+piogru@users.noreply.github.com>
2024-08-22 14:17:05 +0200

    feat: Create JWT utils and add JWT_SECRET to .env example

---
+++ src/utils/auth.util.ts
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+import { Response } from "express";
+import { Types } from "mongoose";
+
+const generateToken = (res: Response, userId: string | Types.ObjectId) => {
+  const jwtSecret = process.env.JWT_SECRET || "";
+  const token = jwt.sign({ userId }, jwtSecret, {
+    expiresIn: "1h",
+  });
+
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "dev",
+    sameSite: "strict",
+    maxAge: 60 * 60 * 1000,
+  });
+};
+
+const clearToken = (res: Response) => {
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+};
+
+export { generateToken, clearToken };
