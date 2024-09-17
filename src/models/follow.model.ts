722a1b85b76e54c830bf9e4b0085600e758260dd
Piotr G <81086063+piogru@users.noreply.github.com>
2024-09-17 12:34:36 +0200

    feat: Create Follow model

---
+++ src/models/follow.model.ts
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from "mongoose";
+
+interface IFollow {
+  follower: Schema.Types.ObjectId;
+  target: Schema.Types.ObjectId;
+}
+
+type FollowInput = {
+  follower: IFollow["follower"];
+  target: IFollow["target"];
+};
+
+const followSchema = new Schema<IFollow>(
+  {
+    follower: { type: Schema.Types.ObjectId, ref: "User" },
+    target: { type: Schema.Types.ObjectId, ref: "User" },
+  },
+  {
+    collection: "follows",
+    timestamps: true,
+  }
+);
+
+const Follow = mongoose.model("Follow", followSchema);
+
+export { IFollow, FollowInput };
+export default Follow;
