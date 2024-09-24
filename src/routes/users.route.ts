1c4709f97174cd851c83261386c9cd3a745c825a
piogru <81086063+piogru@users.noreply.github.com>
2024-09-24 23:46:46 +0200

    Implement GET endpoint for recommended users (#12)
    * refactor: Rename user.route to users.route
    
    * feat: Implement GET route for recommended users

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,9 +1,14 @@
 import express from "express";
-import { getUser, getUsers } from "../controllers/users.controller";
+import {
+  getReccomendedUsers,
+  getUser,
+  getUsers,
+} from "../controllers/users.controller";
 
 const userRouter = express.Router();
 
 userRouter.get("/", getUsers);
+userRouter.get("/recommended", getReccomendedUsers);
 userRouter.get("/:id", getUser);
 
 export default userRouter;
