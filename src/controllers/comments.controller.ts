288adbdeb69c0ed4e69d152009c177e5ef87371a
piogru <81086063+piogru@users.noreply.github.com>
2024-09-25 02:43:59 +0200

    chore: Wrap all controller functions with express-async-handler (#13)

--- src/controllers/comments.controller.ts
+++ src/controllers/comments.controller.ts
@@ -1,48 +1,45 @@
 import { Request, Response } from "express";
+import asyncHandler from "express-async-handler";
 import Comment from "../models/comment.model";
 
-async function getAllUserComments(req: Request, res: Response) {
-  return res.status(501).json({ message: "Not implemented" });
-}
+const getAllUserComments = asyncHandler(async (req: Request, res: Response) => {
+  res.status(501).json({ message: "Not implemented" });
+});
 
-async function getComment(req: Request, res: Response) {
+const getComment = asyncHandler(async (req: Request, res: Response) => {
   const { id } = req.params;
   const comment = await Comment.findOne({ _id: id });
 
   if (!comment) {
-    return res
-      .status(404)
-      .json({ message: `comment with id "${id}" not found.` });
+    res.status(404).json({ message: `comment with id "${id}" not found.` });
   }
 
-  return res.status(200).json(comment);
-}
+  res.status(200).json(comment);
+});
 
-async function updateComment(req: Request, res: Response) {
+const updateComment = asyncHandler(async (req: Request, res: Response) => {
   const { id } = req.params;
   const { content } = req.body;
 
   const comment = await Comment.findOne({ _id: id });
 
   if (!comment) {
-    return res
-      .status(404)
-      .json({ message: `Comment with id "${id}" not found.` });
+    res.status(404).json({ message: `Comment with id "${id}" not found.` });
   }
 
   await Comment.updateOne({ _id: id }, { content: content });
 
   const updatedDocument = await Comment.findById(id);
 
-  return res.status(200).json(updatedDocument);
-}
+  res.status(200).json(updatedDocument);
+});
 
-async function deleteComment(req: Request, res: Response) {
+const deleteComment = asyncHandler(async (req: Request, res: Response) => {
   const { id } = req.params;
 
   await Comment.findByIdAndDelete(id);
 
-  return res.status(200).json({ message: "Comment deleted successfully." });
-}
+  res.status(200).json({ message: "Comment deleted successfully." });
+});
 
 export { getAllUserComments, getComment, updateComment, deleteComment };
--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from "express";
+import asyncHandler from "express-async-handler";
 import User from "../models/user.model";
 
 type UserFilters = {
   email?: string;
 };
 
-const getUsers = async (req: Request, res: Response) => {
+const getUsers = asyncHandler(async (req: Request, res: Response) => {
   const users = await User.find(req.query);
 
   if (!users) {
@@ -13,46 +14,49 @@ const getUsers = async (req: Request, res: Response) => {
   }
 
   res.status(200).json(users);
-};
+});
 
-const getReccomendedUsers = async (req: Request, res: Response) => {
-  const user = req.user;
+const getReccomendedUsers = asyncHandler(
+  async (req: Request, res: Response) => {
+    const user = req.user;
 
-  if (!user) {
-    return res.status(401).json({ message: "Could not identify user" });
-  }
+    if (!user) {
+      res.status(401).json({ message: "Could not identify user" });
+      return;
+    }
 
-  const recommendedUsers = await User.aggregate([
-    { $match: { _id: { $nin: [user._id] } } },
-    { $sample: { size: 5 } },
-    {
-      $lookup: {
-        from: "follows",
-        localField: "_id",
-        foreignField: "target",
-        as: "follow",
-        pipeline: [
-          {
-            $match: { follower: user._id },
-          },
-        ],
+    const recommendedUsers = await User.aggregate([
+      { $match: { _id: { $nin: [user._id] } } },
+      { $sample: { size: 5 } },
+      {
+        $lookup: {
+          from: "follows",
+          localField: "_id",
+          foreignField: "target",
+          as: "follow",
+          pipeline: [
+            {
+              $match: { follower: user._id },
+            },
+          ],
+        },
       },
-    },
-    { $project: { _id: 1, name: 1, profilePic: 1, follow: 1 } },
-    { $unwind: { path: "$follow", preserveNullAndEmptyArrays: true } },
-  ]).exec();
+      { $project: { _id: 1, name: 1, profilePic: 1, follow: 1 } },
+      { $unwind: { path: "$follow", preserveNullAndEmptyArrays: true } },
+    ]).exec();
 
-  if (!recommendedUsers) {
-    res
-      .status(400)
-      .json({ message: "Unable to create a list of recommended users" });
+    if (!recommendedUsers) {
+      res
+        .status(400)
+        .json({ message: "Unable to create a list of recommended users" });
+    }
+
+    res.status(200).json(recommendedUsers);
   }
+);
 
-  res.status(200).json(recommendedUsers);
-};
-
-const getUser = async (req: Request, res: Response) => {
+const getUser = asyncHandler(async (req: Request, res: Response) => {
   res.status(501).json({ message: "Not implemented" });
-};
+});
 
 export { getUsers, getReccomendedUsers, getUser };
