0d3fc3986fe6441f1b5ed475a220e1ddce7a6894
Piotr G <81086063+piogru@users.noreply.github.com>
2024-10-06 23:57:15 +0200

    feat: Setup input validation for follows.controller

--- src/routes/follows.route.ts
+++ src/routes/follows.route.ts
@@ -6,13 +6,33 @@ import {
   getFollowing,
   unfollow,
 } from "../controllers/follows.controller";
+import validateResource from "../middlewares/validateResource.middleware";
+import {
+  followSchema,
+  getCurrentUserFollowSchema,
+  getFollowersSchema,
+  getFollowingSchema,
+  unfollowSchema,
+} from "../schema/follow.schema";
 
 const followsRouter = express.Router();
 
-followsRouter.get("/:targetId", getCurrentUserFollow);
-followsRouter.get("/:targetId/followers", getFollowers);
-followsRouter.get("/:followerId/following", getFollowing);
-followsRouter.post("/:targetId", follow);
-followsRouter.delete("/:targetId", unfollow);
+followsRouter.get(
+  "/:targetId",
+  validateResource(getCurrentUserFollowSchema),
+  getCurrentUserFollow
+);
+followsRouter.get(
+  "/:targetId/followers",
+  validateResource(getFollowersSchema),
+  getFollowers
+);
+followsRouter.get(
+  "/:followerId/following",
+  validateResource(getFollowingSchema),
+  getFollowing
+);
+followsRouter.post("/:targetId", validateResource(followSchema), follow);
+followsRouter.delete("/:targetId", validateResource(unfollowSchema), unfollow);
 
 export default followsRouter;
